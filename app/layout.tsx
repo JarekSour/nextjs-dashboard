df070cd36336dbfbc5faaaf9c828419cb1131176
JarekSour <numetalsour@gmail.com>
2024-01-11 17:45:46 -0300

    step 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { monserrat, inter } from './ui/fonts';
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+
+        <footer className="flex items-center justify-center py-5">
+          Hecho con ❤️
+        </footer>
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Lusitana, Inter } from 'next/font/google'
+
+export const monserrat = Montserrat({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
+export const inter = Inter({ subsets: ['latin'] });
